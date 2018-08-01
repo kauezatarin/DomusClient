399d2cc85242c631dc9fec292bae5ae4802cd044
Kauê Zatarin <kauezatarin@hotmail.com>
2018-08-01 01:16:37 -0300

    Removidas referencias não utilizadas da classe ServerHadler

--- DomusClient/ServerHandler.cs
+++ DomusClient/ServerHandler.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Text;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using DomusClient.Properties;
 
 namespace DomusClient
