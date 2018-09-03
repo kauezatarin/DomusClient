1d74c3487f73f3434badd77ea7f03b26d4a8249c
Kauê Zatarin <kauezatarin@hotmail.com>
2018-09-03 19:54:46 -0300

    Criado formulário de configuração da cisterna.

---
+++ DomusClient/CisternConfigForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace DomusClient
+{
+    public partial class CisternConfigForm : MetroForm
+    {
+        public CisternConfigForm()
+        {
+            InitializeComponent();
+
+            this.BorderStyle = MetroFormBorderStyle.FixedSingle;
+            this.ShadowType = MetroFormShadowType.AeroShadow;
+        }
+    }
+}
